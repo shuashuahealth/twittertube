4d7afbfcf2113fb0c0e9a404e4aea2892e71b4a1
lideminismine <lideminismine@gmail.com>
2014-03-01 03:59:57 -0500

    2

--- twittube/urls.py
+++ twittube/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'twittube_local.views.home', name='home'),
-    # url(r'^twittube_local/', include('twittube_local.foo.urls')),
+     url(r'^$', 'twittube.views.home', name='home'),
+    # url(r'^twittube/', include('twittube.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
