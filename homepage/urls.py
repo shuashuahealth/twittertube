7bc4021b754bb0228c5f7d99306565202cb524b9
lideminismine <lideminismine@gmail.com>
2014-03-02 03:27:55 -0500

    uploadfile test

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,6 +4,6 @@ from homepage import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index')
-	url(r'^handlefile/', views.handlefile, name='handlefile')
+	url(r'^handlefile/$', views.handlefile, name='handlefile')
 )
 
