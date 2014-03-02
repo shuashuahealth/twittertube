14e37c0400c09423451b2d6aef2f36f044e04b26
lideminismine <lideminismine@gmail.com>
2014-03-02 03:46:50 -0500

    uploadfile test

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from homepage import views
 
 urlpatterns = patterns('homepage.views',
-	url(r'^$', views.index, name='index'),
-	url(r'^handlefile/$', views.handlefile, name='handlefile')
+	url(r'^$', 'index', name='index'),
+	url(r'^handlefile/$', 'handlefile', name='handlefile')
 )
 
