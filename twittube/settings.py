45a8b78263c2f58eaa331e9fc0c5b51cef37f795
lideminismine <lideminismine@gmail.com>
2014-03-02 02:39:34 -0500

    uploadfile test

--- twittube/settings.py
+++ twittube/settings.py
@@ -142,6 +142,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'homepage',
+    'storages',
 )
 
 # A sample logging configuration. The only tangible logging
