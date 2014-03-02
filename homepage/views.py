b3928982028a4d41f818a5a503784dd0b2a9f1bf
lideminismine <lideminismine@gmail.com>
2014-03-02 03:32:04 -0500

    uploadfile test

--- homepage/views.py
+++ homepage/views.py
@@ -23,8 +23,8 @@ def handlefile(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
-            return render(request, 'cookbooks/newcb.html', {})
+            return render(request, 'homepage/index.html', {})
     else:
         form = UploadFileForm()
-    return render(request, 'homepage/index.html')
+    return render(request, 'homepage/index.html', {})
 
