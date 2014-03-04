c05fc1ca18735e79edb1a339058d4bb644e411a4
lideminismine <lideminismine@gmail.com>
2014-03-04 01:25:38 -0500

    cloudfront test

--- homepage/models.py
+++ homepage/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Sponsor (model.Model):
+class Sponsor(models.Model):
 	next_int_num = models.IntegerField(default = 1)
 	filename = models.CharField(max_length=100)
 
-class Participant (models.Model):
+class Participant(models.Model):
 	sponsor = models.ForeignKey(Sponsor)
 	internal_num = models.IntegerField(default = 0)
 	filename = models.CharField(max_length=100)
